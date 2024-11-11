0f86be3264a343d01447d7ccc533c8b2ae7d8469
sabir7achraf <sabirachraf032@gmail.com>
2024-11-11 19:35:00 +0100

    before merge

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,10 @@ import {OperationsComponent} from './employee-dashboard/operations/operations.co
 import {GroupsComponent} from './employee-dashboard/groups/groups.component';
 import {DashboardComponent} from './employee-dashboard/dashboard/dashboard.component';
 import {LoginComponent} from './login/login.component';
+import {SupemployeDashboardComponent} from './supemploye-dashboard/supemploye-dashboard.component';
+import {Groups2Component} from './supemploye-dashboard/groups/groups.component';
+import {EmployesComponent} from './supemploye-dashboard/employes/employes.component';
+
 
 export const routes: Routes = [
   {path: 'employee', component: EmployeeDashboardComponent,
@@ -13,13 +17,20 @@ export const routes: Routes = [
     {path: 'dashboards', component: DashboardComponent},
     {path: 'clients', component: UsersComponent},
       {path: 'groups', component: GroupsComponent},
-    {path:'operations', component: OperationsComponent}
+    {path:'operations', component: OperationsComponent},
+      {path:'logout', redirectTo:"/login"}
+    ],
+  },
+  {path: 'Supemployee', component: SupemployeDashboardComponent,
+    children : [
+      {path: 'employes', component: EmployesComponent},
+      {path: 'groups', component: Groups2Component},
+      {path:'logout', redirectTo:"/login"}
     ],
   },
   {path: 'login', component: LoginComponent},
-  //other routes
   {path: '', redirectTo: '/employee/dashboards', pathMatch: 'full'},
-  {path: '**', redirectTo: '/employee/dashboards'} //Wildcards route for unmatched routes
+  {path: '**', redirectTo: '/employee/dashboards'}
   ];
 
 
